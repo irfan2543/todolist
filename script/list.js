0f353de90383896c9129212e19eab271bcc458f8
irfan2543 <muhammadirfansyah66@gmail.com>
2024-12-30 17:17:52 +0700

    Todo List Selesai

--- script/list.js
+++ script/list.js
@@ -14,10 +14,12 @@ Object.keys(localStorage).forEach(data => {
     a.forEach(Todolist => {
         showTodo.innerHTML = 
         `
-         <div class="bg-red-500">
-            <p>${Todolist.titleTodo}</p>
-            <p>${Todolist.commentTodo}</p>
-            <button onclick="deleteTodo(${Todolist.id})">hapus todo</button>
+         <div class="bg-gray-300 m-10 w-52 h-80 flex flex-col text-wrap break-words rounded-xl">
+            <h1 class="mx-2 text-2xl font-bold">${Todolist.titleTodo}</h1>
+            <hr class="border-t-4 border-black my-5">
+            <p class="text-lg bg-gray-300 h-52 font-serif font-medium mx-2 text-justify">${Todolist.commentTodo}</p>
+            <button type="button" onclick="deleteTodo(${Todolist.id})" class="my-5 mx-5 text-white bg-gradient-to-br from-green-400 to-blue-600 hover:bg-gradient-to-bl focus:ring-4 
+            focus:outline-none focus:ring-green-200 dark:focus:ring-green-800 font-medium rounded-lg text-sm py-2.5 text-center ">Telah Dikerjakan</button>
            </div>
         `
     })  
